a50af6c0a4657a31d804900cd9194aba6523e688
AbdulVaqeel <abdulvaqeel9636@gmail.com>
2026-06-02 13:26:58 +0300

    Fixed the backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8001',
+  baseURL: 'https://survey-app-backend-ten.vercel.app/',
   headers: { 'Content-Type': 'application/json' },
 })
 // Attach JWT token to every request automatically
